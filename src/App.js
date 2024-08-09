cbcbed9c0e458facea991bdd54d444dfb4ffd6c6
cole <wuwei@shgbit.com>
2024-08-09 17:37:38 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 
 import './App.css'
 import Home from './componets/Home'
-import ShowcaseLayout from './componets/MyResponsiveGrid'
 
 function App() {
   return (
     <div className="App">
-      {/* <MyFirstGrid /> */}
-      {/* <ShowcaseLayout/> */}
       <Home/>
     </div>
   )
